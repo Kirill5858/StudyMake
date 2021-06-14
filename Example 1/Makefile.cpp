798bcd190220759eab8951832d4885b8adfbdcfd
1234 <1234@LAPTOP-TTUU1C4H>
2021-06-14 18:51:37 +0300

    Добавьте файлы проекта.

---
+++ Example 1/Makefile.cpp
@@ -0,0 +1,11 @@
+# ѕростой make - file без переменных, дл€ релиза и дебага
+.PHONY: all debug release clean
+all : debug release
+debug : hello - dbg
+hello - dbg : hello.cpp
+g++ - g - O0 hello.cpp - o hello - dbg - Wall
+release : hello
+hello : hello.cpp
+g++ - O2 hello.cpp - o hello - DNDEBUG - Wall
+clean :
+rm - fm hello - dbg
---
+++ Example-3/Makefile.cpp
@@ -0,0 +1,16 @@
+# ”сложненный make - file.¬ведены переменные дл€ параметров компил€ции и исполн€емых файлов
+.PHONY: all debug release clean
+RELEASE_FLAGS = -O2 - Wall - DNDEBUG
+DEBUG_FLAGS = -g - O0 - Wall
+RELEASE_EXEC = Kazino - Royal
+DEBUG_EXEC = Kazino - Royal - dbg
+TMP = Kazino - Royal.cpp
+all : debug release
+debug : $(DEBUG_EXEC)
+Kazino - Royal - dbg : $(TMP)
+g++ $(DEBUG_FLAGS)  $(TMP) - o $(DEBUG_EXEC)
+release : $(RELEASE_EXEC)
+Kazino - Royal : $(TMP)
+g++ $(RELEASE_FLAGS)  $(TMP) - o $(RELEASE_EXEC)
+clean :
+	rm - f $(RELEASE_EXEC) $(DEBUG_EXEC)
