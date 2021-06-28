43cc02d9ae66eea274bd83908e7eff8a72806f61
Avocado <82521789+Kirill5858@users.noreply.github.com>
2021-06-28 22:44:32 +0300

    Update Makefile.cpp

--- Example-2/Makefile.cpp
+++ Example-2/Makefile.cpp
@@ -1,16 +1,25 @@
-# Усложненный make - file.Введены переменные для параметров компиляции и исполняемых файлов
+# РЈСЃР»РѕР¶РЅРµРЅРЅС‹Р№ make-file.  Р’РІРµРґРµРЅС‹ РїРµСЂРµРјРµРЅРЅС‹Рµ РґР»СЏ РїР°СЂР°РјРµС‚СЂРѕРІ РєРѕРјРїРёР»СЏС†РёРё Рё РёСЃРїРѕР»РЅСЏРµРјС‹С… С„Р°Р№Р»РѕРІ
+
 .PHONY: all debug release clean
-RELEASE_FLAGS = -O2 - Wall - DNDEBUG
-DEBUG_FLAGS = -g - O0 - Wall
-RELEASE_EXEC = hello
-DEBUG_EXEC = hello - dbg
-TMP = hello.cpp
-all : debug release
-debug : $(DEBUG_EXEC)
-hello - dbg : $(TMP)
-g++ $(DEBUG_FLAGS)  $(TMP) - o $(DEBUG_EXEC)
-release : $(RELEASE_EXEC)
-hello : $(TMP)
-g++ $(RELEASE_FLAGS)  $(TMP) - o $(RELEASE_EXEC)
-clean :
-	rm - f $(RELEASE_EXEC) $(DEBUG_EXEC)
+
+RELEASE_FLAGS = -O2 -Wall -DNDEBUG
+DEBUG_FLAGS   = -g -O0 -Wall
+RELEASE_EXEC  = hello
+DEBUG_EXEC    = hello-dbg
+#SOURCE        =hello
+
+all: debug release
+
+debug: $(DEBUG_EXEC)
+
+hello-dbg: hello.cpp
+	g++ $(DEBUG_FLAGS) hello.cpp -o $(DEBUG_EXEC) 
+
+release: $(RELEASE_EXEC)
+
+hello: hello.cpp
+	g++ $(RELEASE_FLAGS) hello.cpp -o $(RELEASE_EXEC)
+
+clean:
+	rm -f $(RELEASE_EXEC) $(DEBUG_EXEC)
+
